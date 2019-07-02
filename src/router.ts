d00329d33df9c04293ea66ceed7542d4dc53cfe5
sundowndev <raphael@crvx.fr>
2019-07-02 15:44:35 +0200

    refactor: router

--- src/router.ts
+++ src/router.ts
@@ -2,18 +2,21 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Home from './components/Hello.vue';
-import Hello from './components/HelloDecorator.vue';
+import notFound from './components/NotFound.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: '', name: 'home', component: Home },
-  { path: '/hi', name: 'hello', component: Hello },
+  { path: '/hi', name: 'hello', component: notFound },
   {
     path: '/notfound',
     name: 'not_found',
-    component: Hello,
-    beforeEnter: () => {},
+    component: notFound,
+  },
+  {
+    path: '*',
+    redirect: '/notfound',
   },
 ];
 
