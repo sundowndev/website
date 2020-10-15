1b81d3503b3c802d8e563a18857162efff88b8bc
sundowndev <raphael@crvx.fr>
2020-10-15 20:37:44 +0200

    refactor: views, content and style

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export default {
   email: "raphael@crvx.fr",
+  repository: "https://github.com/sundowndev/website",
   links: {
     social: {
       linkedin: "https://www.linkedin.com/in/error-page-not-found/",
