9b622509154b40447509b0a3158e68f9f94b6844
sundowndev <16480203+sundowndev@users.noreply.github.com>
2024-03-12 13:42:23 +0400

    chore: fix typo in content

--- src/views/SponsorsView.vue
+++ src/views/SponsorsView.vue
@@ -48,7 +48,7 @@ export default {
     <h1 class="title-section">Sponsors</h1>
 
     <p>
-      I love contribute to Open Source software! It has always been a goal to
+      I love contributing to Open Source software! It has always been a goal to
       monetize my Open Source activity, so if you would like to support my
       future work, consider joining me as a sponsor!
     </p>
